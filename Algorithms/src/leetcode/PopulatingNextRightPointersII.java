d9126f68814342a3bf3f2d9a07afaa53e89c5815
fq310 <fuqiang.dev@gmail.com>
2014-08-01 15:20:59 +0800

    leetcode oj

---
+++ Algorithms/src/leetcode/PopulatingNextRightPointersII.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class PopulatingNextRightPointersII {
+	public void connect(TreeLinkNode root) {
+		PopulatingNextRightPointers p = new PopulatingNextRightPointers();
+		p.connect(root);
+    }
+}
