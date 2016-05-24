3e454fbbc9395945559e268b761d305c5e1c2694
Jason Fu <jason.fu@booking.com>
2016-05-24 21:14:51 +0800

    Problem 162

---
+++ Algorithms/src/leetcode_152/Problem162.java
@@ -0,0 +1,7 @@
+package leetcode_152;
+
+/**
+ * Created by jafu on 5/24/16.
+ */
+public class Problem162 {
+}
---
+++ Algorithms/test/leetcode_152/Problem162Test.java
@@ -0,0 +1,16 @@
+package leetcode_152;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 5/24/16.
+ */
+public class Problem162Test {
+    @Test
+    public void findPeakElement() throws Exception {
+
+    }
+
+}
